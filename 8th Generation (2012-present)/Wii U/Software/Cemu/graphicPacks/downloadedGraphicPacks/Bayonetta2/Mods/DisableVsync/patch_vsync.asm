009c306b1e8ff0f788d7dd442d0380464f313960
Robb Dilallo <robbdi@outlook.com>
2022-06-21 16:39:20 -0500

    removal and restructure

--- 8th Generation (2012-present)/Wii U/Software/Cemu/graphicPacks/downloadedGraphicPacks/Bayonetta2/Mods/DisableVsync/patch_vsync.asm
+++
@@ -1,5 +0,0 @@
-[Bayo2USv0]
-moduleMatches = 0xAF5D1A85
-.origin = codecave
-
-0x034461C4 = nop #Kill Vsync
--- 8th Generation (2012-present)/Wii U/Software/Cemu/graphicPacks/downloadedGraphicPacks/BreathOfTheWild/Mods/FPS++/patch_LoadingScreen.asm
+++
@@ -1,6 +0,0 @@
-[BotW_LoadingScreen_V208]
-moduleMatches = 0x6267BFD0
-
-.origin = codecave
-
-0x0307CCE0 = nop
