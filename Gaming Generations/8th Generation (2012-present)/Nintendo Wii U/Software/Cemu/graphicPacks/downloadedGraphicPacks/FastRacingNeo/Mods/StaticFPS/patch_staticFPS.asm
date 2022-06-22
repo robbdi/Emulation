2f95fbb4f1925fe168ac9abd12440de7ab39a959
Robb Dilallo <robbdi@outlook.com>
2022-06-22 08:29:34 -0500

    added template readme

--- Gaming Generations/8th Generation (2012-present)/Nintendo Wii U/Software/Cemu/graphicPacks/downloadedGraphicPacks/FastRacingNeo/Mods/StaticFPS/patch_staticFPS.asm
+++
@@ -1,6 +0,0 @@
-[StaticFPS_v65]
-moduleMatches = 0xfab25e0c
-
-0x1000AE10 = .float $targetFPS # Game Speed
-0x1000008C = .float 60/$targetFPS # Animated Objects and Effects Speed
-0x10000090 = .double $targetFPS # GUI Transition Speed
--- Gaming Generations/8th Generation (2012-present)/Nintendo Wii U/Software/Cemu/graphicPacks/downloadedGraphicPacks/SonicBoomRiseOfLyric/Mods/DisableVersionString/patch_DisableVersionString.asm
+++
@@ -1,5 +0,0 @@
-[WiiULauncher16]
-moduleMatches = 0x113CC316
-
-; Overwrite version string with null
-0x102EBF14 = .string ""
